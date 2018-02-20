94577155f5c6108216261db1314e4f52f3719016
Lars Krog-Jensen <lars.krogjensen@gmail.com>
2018-02-20 20:33:07 +0100

    client with reconnect

---
+++ firehose-client/src/main/kotlin/NetClientVerticle.kt
@@ -0,0 +1,65 @@
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.net.NetClient
+import io.vertx.core.net.OpenSSLEngineOptions
+import io.vertx.kotlin.core.net.NetClientOptions
+
+class NetClientVerticle : AbstractVerticle() {
+    val log = logger<NetClientVerticle>()
+    var client: NetClient? = null
+    var timer: Long? = null
+
+    override fun start() {
+        val netClientOptions = NetClientOptions(
+            connectTimeout = 5_000,
+            reconnectAttempts = 10,
+            reconnectInterval = 500,
+            ssl = true,
+            trustAll = true,
+            openSslEngineOptions = if (OpenSSLEngineOptions.isAvailable()) OpenSSLEngineOptions() else null
+
+        )
+        client = vertx.createNetClient(netClientOptions)
+        connect()
+    }
+
+    override fun stop() {
+        log.info("Stopping client")
+        client?.close()
+        timer?.let { vertx.cancelTimer(it) }
+    }
+
+    private fun connect() {
+        log.info("Connecting...")
+        client!!.connect(1234, "localhost") { ar ->
+            if (ar.succeeded()) {
+                log.info("Connected")
+                val socket = ar.result()
+                socket.writeJsonFrame(SessionInitCommand(sequenceNo = 1))
+
+                val parser = frameParser(4) {
+                    log.info("Message received: $it")
+                }
+
+                socket.handler(parser::handle)
+                socket.exceptionHandler { ex ->
+                    log.error("Exception caught: ${ex.message}")
+                    reconnect()
+                }
+                socket.closeHandler {
+                    log.info("Socket closed")
+                }
+            } else {
+                log.error("Failed to connect")
+                reconnect()
+            }
+        }
+    }
+
+    private fun reconnect() {
+        timer = vertx.setTimer(5_000) {
+            log.info("Reconnecting")
+            connect()
+        }
+    }
+}
--- firehose-client/src/main/kotlin/client.kt
+++ firehose-client/src/main/kotlin/client.kt
@@ -1,47 +1,13 @@
+
 import io.vertx.core.Vertx
 import io.vertx.core.net.OpenSSLEngineOptions
 import io.vertx.kotlin.core.VertxOptions
-import io.vertx.kotlin.core.net.NetClientOptions
 
 fun main(args: Array<String>) {
     val vertx = Vertx.vertx(VertxOptions(preferNativeTransport = true))
     println("Using native driver: " + vertx.isNativeTransportEnabled)
     println("OpenSSL is available: " + OpenSSLEngineOptions.isAvailable())
 
-
-    val netClientOptions = NetClientOptions(
-        connectTimeout = 5_000,
-        reconnectAttempts = 10,
-        reconnectInterval = 500,
-        ssl = true,
-        trustAll = true,
-        openSslEngineOptions = if (OpenSSLEngineOptions.isAvailable()) OpenSSLEngineOptions() else null
-
-    )
-    val client = vertx.createNetClient(netClientOptions)
-
-    client.connect(1234, "localhost") { ar ->
-        if (ar.succeeded()) {
-            println("Connected")
-            val socket = ar.result()
-            socket.writeJsonFrame(SessionInitCommand(sequenceNo = 1))
-
-            val parser = frameParser(4) {
-                println("Message received: $it")
-            }
-
-            socket.handler(parser::handle)
-            socket.exceptionHandler { ex ->
-                println("Exception caught: " + ex)
-            }
-            socket.closeHandler {
-                println("Socket closed")
-
-            }
-        } else {
-            println("Failed to connect")
-        }
-    }
-
-    Runtime.getRuntime().addShutdownHook(Thread {client.close()})
+    vertx.deployVerticle(NetClientVerticle())
+    Runtime.getRuntime().addShutdownHook(Thread {vertx.close()})
 }
